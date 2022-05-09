3f3de2c48da25a34f2fa9083bcd54c1d62fc8ca7
Taehyeon130 <75840459+Taehyeon130@users.noreply.github.com>
2022-05-09 17:05:48 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean solution(int x) {
+        int r = x;
+        
+        String n = Integer.toString(x);
+
+        int y = n.length(); 
+
+        int sum = 0;
+        
+       for(int i = y; y>0 ;y--){
+            if(y==1){
+                sum += (x/(10)) + x % ((y*10));
+            }else{
+                int z = (int)Math.pow(10,y);
+                int j = x/z;
+                sum += j;
+                x = x - (z*j);
+            }           
+        }
+       
+      boolean answer = true;
+       if(r%sum != 0){
+            answer = false;
+        }
+        return answer;
+    }
+}
