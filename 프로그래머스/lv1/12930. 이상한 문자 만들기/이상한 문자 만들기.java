e5a468598752f4ad6a142c9ad039b1a2f51db3b2
Taehyeon130 <75840459+Taehyeon130@users.noreply.github.com>
2022-06-22 17:53:29 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.83 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String s) {
         
-        String[] a = s.split("\\s",-1); //공백 기준으로 split해서 배열에
+        String[] a = s.split("\\s",-1);
         String[] c ={};
         StringBuffer b = new StringBuffer();
         for(int i=0;i<a.length;i++){
@@ -17,11 +17,8 @@ class Solution {
             if(i<a.length-1){
                 b.append(" ");                    
             }
-
         }
-         
-       
-        
+    
         String answer = String.valueOf(b);
         return answer;
     }
