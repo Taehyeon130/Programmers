1ec150467ad608de21063f3ca15d5ddffe8e21ad
Taehyeon130 <75840459+Taehyeon130@users.noreply.github.com>
2022-06-28 16:33:53 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        int n = s.length()/2;
+        
+        if(s.length()%2==0){ //짝수라면
+            answer = s.substring(n-1,n+1);
+        }else{
+            answer = s.substring(n,n+1);
+        }
+        
+        return answer;
+    }
+}
