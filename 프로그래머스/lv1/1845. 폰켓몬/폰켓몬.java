87702ae7a5361ce41857a325d8155801cad5be11
Taehyeon130 <75840459+Taehyeon130@users.noreply.github.com>
2022-06-30 14:06:10 +0900

    [level 1] Title: 폰켓몬, Time: 4.15 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,26 +1,11 @@
-import java.util.*;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] nums) {
-          int answer = 0;
-        List<Integer>list = Arrays.stream(nums)
-                                    .boxed()
-                                    .collect(Collectors.toList());
-        
-        list =  list.stream().distinct().collect(Collectors.toList());
-        System.out.println(list);
-        
-        //list의 사이즈가 포켓몬 종류
-        //nums.length/2가 가져갈 수 있는 포켓몬 수
-        
-        
-        if(nums.length/2 >list.size()){ //포켓몬 종류가 3마린데 2개밖에 못가져갈때
-            answer = list.size();
-        }else{
-            answer= nums.length/2;
-        }
-       
-        return answer;
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.collectingAndThen(Collectors.toSet(),
+                        phonekemons -> Integer.min(phonekemons.size(), nums.length / 2)));
     }
 }
