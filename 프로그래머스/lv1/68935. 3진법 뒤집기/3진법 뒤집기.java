cfad36c98e4a05de7c1dc91ea31d4ebe7dadb412
Taehyeon130 <75840459+Taehyeon130@users.noreply.github.com>
2022-06-30 11:00:40 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.15 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while (n >= 3){
+            sb.append(n%3);
+            n = n/3;
+        }        
+        sb.append(n%3);
+
+        String s = sb.toString();
+        String[] x = s.split("");
+        int j = 1;
+        
+        for(int i=x.length-1;i>-1;i--){
+            int m = Integer.parseInt(x[i]);
+            answer = answer + m*j;
+               j = j*3;
+        }
+
+        return answer;
+    }
+}
