28b418ff2d86128e3f5b5272b3766d5c0c8c21d9
Taehyeon130 <75840459+Taehyeon130@users.noreply.github.com>
2022-07-01 11:04:19 +0900

    [level 1] Title: K번째수, Time: 0.34 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -8,18 +8,16 @@ class Solution {
         List<Integer>list = new ArrayList<>(); 
         
         for(int i=0;i<commands.length;i++){
-            for(int j=0;j<commands[i].length;j++){
-                int[] n = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
-                list = Arrays.stream(n).boxed().collect(Collectors.toList());
-                list.sort(Comparator.naturalOrder());                
-            }
+            int[] n = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+            Arrays.sort(n);
+            answer[i] = n[commands[i][2]-1];
+            //list = Arrays.stream(n).boxed().collect(Collectors.toList());
+            //list.sort(Comparator.naturalOrder());                
             
-            answer[i] = list.get(commands[i][2]-1);
+            
+            //answer[i] = list.get(commands[i][2]-1);
         }
-        
-        
-        
-        
+
         return answer;
     }
 }
