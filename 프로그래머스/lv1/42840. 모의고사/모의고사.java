88c1ff3fcee7757eda86ba15833bddedd42458cf
Taehyeon130 <75840459+Taehyeon130@users.noreply.github.com>
2022-07-01 10:27:35 +0900

    [level 1] Title: 모의고사, Time: 15.22 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution { //answers의 길이만큼 나머지 1,2,3의 크기가 달라져야지
+    public int[] solution(int[] answers) {
+        int[] answer = {}; 
+        
+        int len = answers.length;
+        
+        int[] a = {1,2,3,4,5};
+        int[] b = {2,1,2,3,2,4,2,5};
+        int[] c = {3,3,1,1,2,2,4,4,5,5};
+        
+        int aCnt=0;
+        int bCnt=0;
+        int cCnt=0;
+        
+        for(int i=0;i<answers.length;i++){
+            if(a[i%a.length] == answers[i]) aCnt++;
+            if(b[i%b.length] == answers[i]) bCnt++;
+            if(c[i%c.length] == answers[i]) cCnt++;
+        }
+    
+        System.out.println("1번 "+aCnt+" 2번 "+bCnt+" 3번 "+cCnt);
+        
+        int  max = Math.max(aCnt,Math.max(bCnt,cCnt));
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        if(max == aCnt){list.add(1);}
+        if(max == bCnt){list.add(2);}
+        if(max == cCnt){list.add(3);}
+        
+        System.out.println(list);
+        
+        answer = list.stream().mapToInt(i-> i.intValue()).toArray();
+        
+        
+        
+        return answer;
+    }
+}
