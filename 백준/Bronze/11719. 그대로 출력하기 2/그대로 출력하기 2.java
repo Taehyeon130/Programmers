ad45bb376cab3118340176cc57d944e0530c81e4
Taehyeon130 <75840459+Taehyeon130@users.noreply.github.com>
2025-01-15 00:02:36 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 200 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] data){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String test = sc.nextLine();
+            System.out.println(test);    
+        }
+    }
+}
