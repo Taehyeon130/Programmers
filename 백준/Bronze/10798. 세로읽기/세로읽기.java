8fc8f556cc76952e3c9f354189e26bee403ef430
Taehyeon130 <75840459+Taehyeon130@users.noreply.github.com>
2025-01-17 23:40:53 +0900

    [Bronze I] Title: 세로읽기, Time: 104 ms, Memory: 14480 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        int maxLength=0;
+        
+        String[][] carr = new String[5][15];
+
+        for(int i=0;i<5;i++){
+            String[] S = br.readLine().split("");
+            
+            if(maxLength < S.length){
+                maxLength = S.length;
+            }
+            
+           for(int j=0;j<S.length;j++){
+               carr[i][j] = S[j];
+           }
+        }
+        
+
+        for(int i=0;i<maxLength;i++){
+            for(int j=0;j<5;j++){
+                if(carr[j][i] != null){
+                    sb.append(carr[j][i]);
+                }
+                
+            }
+        }
+        
+        System.out.print(sb);
+    }
+}
