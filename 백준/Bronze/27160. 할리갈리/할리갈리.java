0bc6f6aee3385635aaaf162b389e36c18f7af1ee
Taehyeon130 <75840459+Taehyeon130@users.noreply.github.com>
2025-01-21 00:23:58 +0900

    [Bronze II] Title: 할리갈리, Time: 412 ms, Memory: 48776 KB -BaekjoonHub

--- 백준/Bronze/27160. 할리갈리/할리갈리.java
+++ 백준/Bronze/27160. 할리갈리/할리갈리.java
@@ -6,7 +6,7 @@ class Main {
     public static void main(String[] args) throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<String,String> h1 = new HashMap<String,String>();
+        HashMap<String,Integer> h1 = new HashMap<String,Integer>();
         
         //카드의 개수
         String N = br.readLine();
@@ -16,26 +16,19 @@ class Main {
             
                 //해시맵에 포함되어있다면
                 if(h1.containsKey(S[0])){
-                    int n = Integer.parseInt(h1.get(S[0]));
+                    int n = h1.get(S[0]);
                     int s = Integer.parseInt(S[1]) + n;
-                    h1.put(S[0],Integer.toString(s));
+                    h1.put(S[0],s);
                 }else{
-                    h1.put(S[0],S[1]);
+                    h1.put(S[0],Integer.parseInt(S[1]));
                 }
             
         }
-
-        String answer = "";
-        for (String key: h1.keySet()) {
-			String value = h1.get(key);
-            if(value.equals("5")){
-                answer = "YES";
-               break;
-            }else{
-                answer = "NO";
-            }
-		}
-
-        System.out.println(answer);
+       
+        if(h1.containsValue(5)){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
     }
 }
